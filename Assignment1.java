dfb3b214b418960b2efc8cbf6b0640bf94f5ab24
Saif-12 <47641013+Saif-12@users.noreply.github.com>
2024-02-09 23:47:35 +0530

    Add files via upload

--- Assignment1.java
+++ Assignment1.java
@@ -1,6 +1,40 @@
+package SortingAlgorithmAssignment;
 
 public class Assignment1 {
+	
+	public static void print(int arr[])
+	{
+		int n=arr.length;
+		for(int i =0;i<n;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello world!!");
+		
+		int arr[]= {3,1,2,5,4};   //sort in decreasing order using bubble sort
+		print(arr);
+		
+		int n=arr.length;
+		for(int i=0;i<n-1;i++)
+		{
+			boolean flag=true;
+			for(int j=0;j<n-1-i;j++)
+			{
+				if(arr[j]<arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+					flag=false;
+				}
+			}
+			if(flag==true) break;
+		}
+		
+		print(arr);
+
 	}
 }
